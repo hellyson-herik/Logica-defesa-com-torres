ff65fa5def2d30b28617d405bbe64ba8447bd73f
Hellyson Herik <hherik96@gmail.com>
2023-12-07 06:30:28 -0300

    resolução até chegar no modelo satisfatorio

--- iniciando_novamente_com_z3.py
+++ iniciando_novamente_com_z3.py
@@ -5,12 +5,14 @@ solver = Solver()
 
 
 def lendo_mapa(mapa):
+
     for linha in range(len(mapa)):
         for coluna in range(len(mapa[0])):
             if mapa[linha][coluna] == 'n':
                 solver.add(formula_atacantes(mapa, linha, coluna))
             if mapa[linha][coluna] == 'T':
                 solver.add(formula_torre(mapa, linha, coluna))
+    return solver
 
 
 def formula_atacantes(mapa, linha_atacante, coluna_atacante):
@@ -37,7 +39,7 @@ def formula_torre(mapa, linha_torre, coluna_torre):
     lista_torre = []
     castelo = False
     for linha in range(len(matriz)):
-        if mapa[linha][coluna_torre] == 'T':
+        if mapa[linha][coluna_torre] == 'T' and linha != linha_torre:
             if linha < linha_torre:
                 for linha_2 in range(linha, linha_torre):
                     if mapa[linha_2][coluna_torre] == '#':
@@ -56,7 +58,7 @@ def formula_torre(mapa, linha_torre, coluna_torre):
                     lista_torre.append(Not(Bool(f'T_{linha_torre + 1}_{coluna_torre + 1}_c')))
 
     for coluna in range(len(matriz[0])):
-        if mapa[linha_torre][coluna] == 'T':
+        if mapa[linha_torre][coluna] == 'T' and coluna != coluna_torre:
             if coluna < coluna_torre:
                 for coluna_2 in range(coluna, coluna_torre):
                     if mapa[linha_torre][coluna_2] == '#':
@@ -74,7 +76,27 @@ def formula_torre(mapa, linha_torre, coluna_torre):
                 else:
                     lista_torre.append(Not(Bool(f'T_{linha_torre + 1}_{coluna_torre + 1}_e')))
 
+    lista_and = [And([f for f in lista_torre])]
+    return lista_and
 
+
+def escolhendo_canhoes(mapa, solucao):
+    for linha in range(len(mapa)):
+        for coluna in range(len(mapa[0])):
+            if mapa[linha][coluna] == 'T':
+                for cont in range(len(solucao)):
+                    if solucao[Not(Bool(f'T_{linha+1}_{coluna+1}_c'))] and solucao[Bool(f'T_{linha+1}_{coluna+1}_e')]:
+                        mapa[linha][coluna] = '1'
+                    elif (solucao[Not(Bool(f'T_{linha+1}_{coluna+1}_c'))] and
+                          solucao[Not(Bool(f'T_{linha+1}_{coluna+1}_e'))]):
+                        mapa[linha][coluna] = '2'
+                    elif (solucao[Bool(f'T_{linha+1}_{coluna+1}_c')] and
+                          solucao[Not(Bool(f'T_{linha+1}_{coluna+1}_e'))]):
+                        mapa[linha][coluna] = '3'
+                    elif (solucao[Bool(f'T_{linha+1}_{coluna+1}_c')] and
+                          solucao[Bool(f'T_{linha+1}_{coluna+1}_e')]):
+                        mapa[linha][coluna] = '4'
+    return mapa
 maps = [
     '. n . . T . . n .',
     '. T . . n . . . .',
@@ -83,5 +105,21 @@ maps = [
     '. n . . T . . n .'
 ]
 matriz = criando_matriz(maps)
-lista_or_atacante_1 = formula_atacantes(matriz, 0, 1)
-print(lista_or_atacante_1)
+solucao = lendo_mapa(matriz)
+resposta = solucao.check()
+if resposta == sat:
+    print('O mapa é Satisfatível.')
+else:
+    print('O mapa não Satisfativel.')
+
+solucao = solucao.model()
+# solucao = solucao[0].as_long()
+
+print(solucao)
+# for n in range(len(solucao)):
+#     if solucao[n] == 'T_4_8_c = True':
+#         print('verdadeiro')
+#     elif solucao[n] == 'T_2_2_e = False':
+#         print('falso')
+# mapa_com_canhoes = escolhendo_canhoes(matriz, solucao)
+# print_matriz(mapa_com_canhoes)
